74cf1fc3b8f2352aab2da4492e41798c939a2973
Angold-4 <wangold4w@gmail.com>
2022-04-06 00:29:08 +0800

    add sub support

--- acc/acc.c
+++ acc/acc.c
@@ -3,14 +3,34 @@
 
 int main(int argc, char **argv) {
   if (argc != 2) {
-    fprintf(stderr, "引数の個数が正しくありません\n");
+    fprintf(stderr, "Args Err!\n");
     return 1;
   }
+
+  char *p = argv[1]; // char pointer
 
   printf(".intel_syntax noprefix\n");
   printf(".globl _main\n");
   printf("_main:\n");
-  printf("  mov rax, %d\n", atoi(argv[1]));
+  printf("  mov rax, %ld\n", strtol(p, &p, 10)); // base 10
+
+  while (*p) {
+      if (*p == '+') {
+	  p++;
+	  printf("  add rax, %ld\n", strtol(p, &p, 10));
+	  continue;
+      }
+
+      if (*p == '-') {
+	  p++;
+	  printf("  sub rax, %ld\n", strtol(p, &p, 10));
+	  continue;
+      }
+
+      fprintf(stderr, "invalid input: %c\n", *p);
+      return 1;
+  }
+
   printf("  ret\n");
   return 0;
 }
