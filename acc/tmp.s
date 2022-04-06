81f838a303cf265d3bb6f7e6419527ecc866b56c
Angold-4 <wangold4w@gmail.com>
2022-04-07 00:28:37 +0800

    improve error message

--- acc/tmp.s
+++ acc/tmp.s
@@ -1,5 +1,5 @@
-	.section	__TEXT,__text,regular,pure_instructions
-	.build_version macos, 12, 0	sdk_version 12, 0
+.section	__TEXT,__text,regular,pure_instructions
+.build_version macos, 12, 0	sdk_version 12, 0
 .globl _main
 _main:
   mov $5, %rax
